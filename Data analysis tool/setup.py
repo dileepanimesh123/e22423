2e151e535aec8334539630ef54f5e63592aecfcb
dileepanimesh123 <e22423@eng.pdn.ac.lk>
2026-06-02 22:31:05 +0530

    Update setup.py

--- Data analysis tool/setup.py
+++ Data analysis tool/setup.py
@@ -1,1 +1,16 @@
+from setuptools import setup, find_packages
 
+setup(
+    name="data_analysis_tool",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy",
+        "plotly",
+        "scikit-learn",
+        "scipy",
+        "pydantic",
+        "statsmodels"
+    ],
+)
